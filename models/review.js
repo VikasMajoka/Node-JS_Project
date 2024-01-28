b46e6bb0eef395cd1d43215214e4c0ebe3717c86
Vikas <majokavikas@gmail.com>
2024-01-29 00:52:09 +0530

    Create Review Model

--- models/review.js
+++ models/review.js
@@ -1,5 +1,15 @@
-/*
-Review
-body -string
-author -object id
-*/
+const mongoose = requie('mongoose');
+const Schema = mongoose.Schema ;
+
+const ReviewSchema = newSchema({
+  body:String,
+
+  author: {
+    type:Schema.Types.ObjectId,
+    ref:'user'
+  }
+     
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
+
