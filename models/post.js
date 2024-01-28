6075efa1721058c620d48faaaebf7146421b8082
Vikas <majokavikas@gmail.com>
2024-01-29 00:11:24 +0530

    Create User model

---
+++ models/post.js
@@ -0,0 +1,12 @@
+/*
+Post
+title -sting
+price -string
+description -string
+images -array of strings
+location -string
+lat -number
+lng -number
+author -object id
+review - array of objects
+*/
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = requie('mongoose');
+const passportLocalMongoose =require('passport-local-mongoose');
+const Schema = mongoose.Schema ;
+
+const UserSchema = newSchema({
+  email:string,
+  image:string,
+  posts:[
+    {
+        type:Schema.Types.objectId,
+        ref: 'Post'
+
+  }] 
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
+
