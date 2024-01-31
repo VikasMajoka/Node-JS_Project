8354918dab841e59a547ee4b9ecdc58965aa4ef7
Vikas <majokavikas@gmail.com>
2024-02-01 00:39:40 +0530

    Add User Email & Password

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,14 @@
-const user =  require('../models/user');
+const User =  require('../models/user');
 module.exports = {
     postRegister(req , res , next){
-        user.register(new user({username: req.body.username}), req.body.password, (err) => {
-            if (err) {
-              console.log('error while user register!', err);
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        image: req.body.image
+      });  
+      User.register (newUser, req.body.password, (err) => {
+        if (err){
+            console.log('error while user register!', err);
               return next(err);
             }
         
