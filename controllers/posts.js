79c113f8738ada87c80650ef115c41bc3d71defa
Vikas <majokavikas@gmail.com>
2024-02-02 23:39:02 +0530

    New Post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,13 @@
 const Post = require('../models/post');
 
 module.exports = {
+   //posts index
  async getPosts (req ,res ,next) {
     let posts = await Post.find({});
     res.render('posts/index',{ posts })
+ },
+ //Posts new
+ newPost(req ,res , next){
+   res.render('posts/new')
  }
 }
