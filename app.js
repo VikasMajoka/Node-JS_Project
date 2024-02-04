3a89b852bafe4dcc4aa7fa11e00b5363ae2d6f78
Vikas <majokavikas@gmail.com>
2024-02-04 14:58:32 +0530

    Posts-edit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const User = require('./models/user');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 //require routes
 const indexRouter = require('./routes/index');
@@ -33,6 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 //Configue passport and sessions
 app.use(session({
